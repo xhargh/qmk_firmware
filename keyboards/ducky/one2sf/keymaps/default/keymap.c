c88458f4f5beb08cfc64eaf5fe9a8e33c9544c55
Khairullah <mkhairullah430@gmail.com>
2022-09-20 11:54:04 +0700

    add new layout WINX

--- keyboards/ducky/one2sf/keymaps/default/keymap.c
+++ keyboards/ducky/one2sf/keymaps/default/keymap.c
@@ -31,6 +31,7 @@ enum Layer {
     HYBRIDFN,
     MAC,
     MACFN,
+    WINX,
     FN  
 };
 #define _QW _QWERTY
@@ -113,10 +114,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, MO(FN),                              _______,                     MO(FN), _______, _______,  KC_HOME, KC_PGDN, KC_END
     ),
 
+    [WINX] = LAYOUT_all(
+        //       2        3        4        5        6        7        8        9        10       11       12       13       14         15
+        KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+        MO(WINFN),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_PGDN,
+        KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
+        KC_LGUI,    KC_LALT, KC_LCTL,                            KC_SPC,                    KC_RCTL, KC_RALT, MO(WINFN) ,  KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
     [FN] = LAYOUT_all(
         KC_ESC, KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
         KC_CAPS, DF(MAC), DF(WIN), KC_FLXP, KC_TASK, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, NK_TOGG,
-        _______, DF(HYBRID), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG,
+        _______, DF(HYBRID), DF(WINX), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG,
         _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, KC_MSTP,
         _______, _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT
     ),
