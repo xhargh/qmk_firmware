1d9e75f065b77a96a17b0bb44ebf74c40c7e0656
Gustav Andersson <xhargh@gmail.com>
2022-11-05 16:32:08 +0100

    Disable space cadet, added :: and ``` button

--- keyboards/keychron/q1/q1_iso_stm32l432/keymaps/xhargh/keymap.c
+++ keyboards/keychron/q1/q1_iso_stm32l432/keymaps/xhargh/keymap.c
@@ -28,6 +28,7 @@
 // Right key cluster with FN, are toggle RGB, lower intensity, higher intensity
 
 #include QMK_KEYBOARD_H
+#include "sendstring_us_international.h"
 
 // clang-format off
 
@@ -42,7 +43,8 @@ enum custom_keycodes {
     KC_MISSION_CONTROL = SAFE_RANGE,
     KC_LAUNCHPAD,
     KC_TASK_VIEW,
-    KC_FILE_EXPLORER
+    KC_FILE_EXPLORER,
+    KC_2X_COLON
 };
 
 typedef struct PACKED {
@@ -56,6 +58,7 @@ key_combination_t key_comb_list[2] = {{2, {KC_LWIN, KC_TAB}}, {2, {KC_LWIN, KC_E
 #define KC_LPAD KC_LAUNCHPAD
 #define KC_TASK KC_TASK_VIEW
 #define KC_FLXP KC_FILE_EXPLORER
+#define KC_2COL KC_2X_COLON
 
 enum {
     TD_ARING = 0,
@@ -94,8 +97,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   DYN_REC_START1,     DYN_MACRO_PLAY1,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_DEL,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     ARING,      KC_RBRC,                      KC_HOME,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     OUML,     AUML,       C(KC_C),  KC_ENT,             KC_END,
-        KC_LSPO,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSPC,  KC_UP,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     OUML,     AUML,       KC_2COL,  KC_ENT,             KC_END,
+        KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 
@@ -119,25 +122,77 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // clang-format on
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    static bool rshifted = false;
+    static bool lshifted = false;
+
     switch (keycode) {
-        case KC_MISSION_CONTROL:
+        case KC_LSFT: {
+            if (record->event.pressed) {
+                lshifted = true;
+            }
+            else {
+                lshifted = false;
+            }
+            return true;
+        }
+        case KC_RSFT:{
+            if (record->event.pressed) {
+                rshifted = true;
+            }
+            else {
+                rshifted = false;
+            }
+            return true;
+        }
+        
+        case KC_2COL:
+            if (record->event.pressed) {
+                if (!lshifted && !rshifted)
+                {
+                    SEND_STRING("::");
+                }
+                else
+                {
+                    // Trying to work around shifted state to actually send ```
+                    if (rshifted) {
+                        unregister_code(KC_RSFT);
+                    }
+                    if (lshifted) {
+                        unregister_code(KC_LSFT);
+                    }
+
+                    SEND_STRING("```");
+                    
+                    if (rshifted) {
+                        register_code(KC_RSFT);
+                    }
+                    if (lshifted) {
+                        register_code(KC_LSFT);
+                    }
+                }
+            }
+            return false;  // Skip all further processing of this key
+        case KC_MISSION_CONTROL: {
             if (record->event.pressed) {
                 host_consumer_send(0x29F);
             } else {
                 host_consumer_send(0);
             }
             return false;  // Skip all further processing of this key
-        case KC_LAUNCHPAD:
+        }
+        case KC_LAUNCHPAD: {
             if (record->event.pressed) {
                 host_consumer_send(0x2A0);
             } else {
                 host_consumer_send(0);
             }
             return false;  // Skip all further processing of this key
+        }
         case KC_TASK:
-        case KC_FLXP:
+        case KC_FLXP: // fall through
+        {
             if (record->event.pressed) {
                 for (uint8_t i = 0; i < key_comb_list[keycode - KC_TASK].len; i++) {
                     register_code(key_comb_list[keycode - KC_TASK].keycode[i]);
@@ -148,6 +203,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;  // Skip all further processing of this key
+        }
         default:
             return true;   // Process all other keycodes normally
     }
--- keyboards/keychron/q1/q1_iso_stm32l432/keymaps/xhargh/rules.mk
+++ keyboards/keychron/q1/q1_iso_stm32l432/keymaps/xhargh/rules.mk
@@ -2,4 +2,3 @@ SRC += factory/major/test.c
 DYNAMIC_MACRO_ENABLE = yes
 # DYNAMIC_TAPPING_TERM_ENABLE = yes
 TAP_DANCE_ENABLE = yes
-COMMAND_ENABLE = no # to not interfere with space cadet shift-()
